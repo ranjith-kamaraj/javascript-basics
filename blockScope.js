1797ce7ea79ed5f74fc903f7bb499747d884438a
Ranjith Kamaraj <42859314+ranjith18996@users.noreply.github.com>
2022-08-19 19:48:25 +0530

    Update blockScope.js

--- blockScope.js
+++ blockScope.js
@@ -1,6 +1,14 @@
-Block Scope also called as compound statement
+let and const - Block Scope -> Will be stored in 'separate memory' of block scope
+var - Function Scope
 
-{
-
-
+if(true) {
+    //Comound Statement - Group of statements
+    let a = 10;
+    console.log(a);
+    
 }
+
+Where javascript expects single statements, there will provide group of statements
+Block Scope - What are functions and variables accepts inside block as block scope
+
+
